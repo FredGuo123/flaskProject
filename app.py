d5a0d19718b1e7339d8203f3ec62d9881f3c3001
Fred <1498880193@qq.com>
2022-10-16 16:06:50 +0100

    add a /register/ path

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import random
+
 from flask import Flask,render_template,request,flash
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import pandas as pd
+from datetime import datetime
 pymysql.install_as_MySQLdb()
 
 
@@ -15,7 +18,38 @@ db = SQLAlchemy(app)
 # 注册
 @app.route('/register/',methods=['GET','POST'])
 def register():
-    return "成功！"
+    if request.method == 'POST':
+        json = request.get_json()
+        email = json['email']
+        password1 = json['password1']
+        password2 = json['password2']
+        type = json['type']
+        name = json['name']
+        last_name = json['last_name']
+        driver_no = json['driver_no']
+        expiry_date = json['expiry_date']
+        # expiry_date = (datetime.strptime(expiry_date, '%Y/%m/%d')).date()
+        contact = json['contact']
+        emergency_name = json['emergency_name']
+        emergency_contact = json['emergency_contact']
+        dataframe = pd.DataFrame()
+        while dataframe.empty:
+            sql1 = "SELECT COUNT(*) FROM customer"
+            result1 = db.session.execute(sql1).fetchall()
+            dataframe = pd.DataFrame(result1)
+            cus_id_num = dataframe.at[0, 'COUNT(*)']
+            cus_id = 'GLA' + str(cus_id_num)
+
+            if email!=None and password1!=None and password2!=None:
+                if password1 == password2:
+                    sql1 = "INSERT INTO customer(cus_id, email, password, type, name, last_name, driver_no, expiry_date, contact, emergency_name, emergency_contact) VALUES ( '{}', '{}', '{}', {}, '{}', '{}', '{}', '{}', '{}', '{}', '{}')" .format(cus_id, email, password1, type, name, last_name, driver_no, expiry_date, contact, emergency_name, emergency_contact)
+                    db.session.execute(sql1)
+                    db.session.commit()
+                else:
+                    print('You password is not same!')
+            else:
+                print('You enter is wrong!')
+    return "Finish！"
 
 
 
@@ -49,7 +83,7 @@ def forget():
     if request.method == 'POST':
         json = request.get_json()
         email = json['email']
-        vercode = json['vercode']
+        # vercode = json['vercode']
         password1 = json['password1']
         password2 = json['password2']
         if email != None:
